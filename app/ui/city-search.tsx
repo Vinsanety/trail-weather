f2e87c1c6ca6455db9166a3621719cf308969695
Vince Vahala <vincevahala@gmail.com>
2024-02-28 09:10:49 -0700

    layout update. styling

--- app/ui/city-search.tsx
+++ app/ui/city-search.tsx
@@ -1,7 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import { useState } from "react";
-import { Moon, Sunrise, Sunset } from "lucide-react";
+import {
+  CloudRain,
+  Droplets,
+  Moon,
+  Snowflake,
+  Sun,
+  Sunrise,
+  Sunset,
+  Wind,
+} from "lucide-react";
 
 interface WeatherData {
   forecast: any;
@@ -46,9 +55,26 @@ export default function CitySearch() {
     }
   };
 
-  const currentTime = new Date()
-    .toLocaleTimeString()
-    .replace(/(.*)\D\d+/, "$1");
+  const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString().replace(/(.*)\D\d+/, "$1");
+  const currentMonth = months[date.getMonth()];
+  const currentDay = days[date.getDay()];
+  const dayOfMonth = date.getDate();
 
   return (
     <>
@@ -92,19 +118,27 @@ export default function CitySearch() {
         </div>
       </div>
       {weatherData && (
-        // <div className="grid grid-flow-row auto-rows-max">
         <div className="container mx-auto">
           <div className="columns-1 gap-4 lg:columns-2 grid lg:flex">
             <div className="stats border-2 rounded-2xl border-slate-100 my-2 lg:my-8 mx-auto w-5/6 lg:w-2/6 bg-base-200 shadow-lg">
               <div className="stat place-items-center mx-auto">
                 <h2 className="stat-title flex mb-2 mx-auto justify-center items-center flex-wrap text-2xl">
-                  {weatherData.location.name}, {weatherData.location.region}{" "}
-                  <img alt="" src={weatherData.current.condition.icon} />
+                  {weatherData.location.name}, {weatherData.location.region}
                 </h2>
-                <p className="stat-value mx-auto justify-center">
-                  {weatherData.current.temp_f}°F
+                <p className="flex justify-evenly w-full">
+                  {currentDay}, {currentMonth} {dayOfMonth}
+                  <span>{currentTime}</span>
+                </p>
+                <p className="stat-value flex items-center mt-4 mx-auto justify-center">
+                  {Math.round(weatherData.current.temp_f)}°F
+                  <img alt="" src={weatherData.current.condition.icon} />
                 </p>
                 <p className="mx-auto">{weatherData.current.condition.text}</p>
+                <p className="flex mx-auto">
+                  <Wind />
+                  {weatherData.current.wind_dir}
+                  {Math.round(weatherData.current.wind_mph)} mph
+                </p>
                 <div className="stat-desc flex justify-between w-full mt-8">
                   <p>
                     <b>High:</b>{" "}
@@ -149,12 +183,12 @@ export default function CitySearch() {
                       {weatherData.current.gust_mph}mph
                     </span>
                   </p>
-                  <p className="flex justify-between">
+                  {/* <p className="flex justify-between">
                     <b>Humidity:</b>{" "}
                     <span className="text-base-content">
                       {weatherData.current.humidity}%
                     </span>
-                  </p>
+                  </p> */}
                   <p className="flex justify-between">
                     <b>UV Index:</b>{" "}
                     <span className="text-base-content">
@@ -209,32 +243,48 @@ export default function CitySearch() {
           </div>
           <div className="stats border-2 rounded-2xl border-slate-100 flex flex-col lg:flex-row mb-8 mx-auto shadow-lg bg-base-200">
             <div className="stat pb-0 lg:pb-4">
-              <div className="stat-figure text-secondary">
-                <Sunrise />
-              </div>
               <div className="stat-title">Sunrise</div>
-              <div className="stat-value text-lg">
+              <div className="stat-value text-lg flex mb-2">
                 {weatherData.forecast.forecastday[0].astro.sunrise}
+                <Sunrise className="ml-4 stroke-secondary" />
+              </div>
+              <div className="stat-title">Sunset</div>
+              <div className="stat-value text-lg flex">
+                {weatherData.forecast.forecastday[0].astro.sunset}
+                <Sunset className="ml-4 stroke-secondary" />
               </div>
             </div>
             <div className="stat pb-0 lg:pb-4">
-              <div className="stat-figure text-secondary">
-                <Sunset />
+              <div className="stat-title">Humidity</div>
+              <div className="stat-value text-lg flex mb-2">
+                {weatherData.current.humidity}%
+                <Droplets className="ml-4 stroke-secondary" />
               </div>
-              <div className="stat-title">Sunset</div>
-              <div className="stat-value text-lg">
-                {weatherData.forecast.forecastday[0].astro.sunset}
+              <div className="stat-title">UV Index</div>
+              <div className="stat-value text-lg flex">
+                {weatherData.current.uv} of 11
+                <Sun className="ml-4 stroke-secondary" />
+              </div>
+            </div>
+            <div className="stat pb-0 lg:pb-4">
+              <div className="stat-title">Chance of rain</div>
+              <div className="stat-value text-lg flex mb-2">
+                {weatherData.forecast.forecastday[0].day.daily_chance_of_rain}%
+                <CloudRain className="ml-4 stroke-secondary" />
+              </div>
+              <div className="stat-title">Chance of snow</div>
+              <div className="stat-value text-lg flex">
+                {weatherData.forecast.forecastday[0].day.daily_chance_of_snow}%
+                <Snowflake className="ml-4 stroke-secondary" />
               </div>
             </div>
             <div className="stat pb-2 lg:pb-4">
-              <div className="stat-figure text-secondary">
-                <Moon />
-              </div>
               <div className="stat-title">Moon Phase</div>
-              <div className="stat-value text-lg">
+              <div className="stat-value text-lg flex mb-2">
                 {weatherData.forecast.forecastday[0].astro.moon_phase}
+                <Moon className="ml-4 stroke-secondary" />
               </div>
-              <div className="flex justify-between w-full">
+              <div className="flex justify-between flex-wrap w-full">
                 <div className="stat-desc">
                   Moonrise: {weatherData.forecast.forecastday[0].astro.moonrise}
                 </div>

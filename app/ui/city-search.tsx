34b1540cb647bf3e38b0bc1ef621a82e4beb665a
Vince Vahala <vincevahala@gmail.com>
2025-11-19 09:43:36 -0700

    added info icon to alert message

--- app/ui/city-search.tsx
+++ app/ui/city-search.tsx
@@ -166,8 +166,21 @@ export default function CitySearch() {
       {errorMsg && (
         <div
           role="alert"
-          className="alert my-4 lg:mt-24 mx-auto w-10/12 lg:w-6/12 whitespace-pre-line"
+          className="alert alert-warning my-4 lg:mt-24 mx-auto w-10/12 lg:w-6/12 whitespace-pre-line"
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
           <span className="pb-4 sm:pb-0">{errorMsg}</span>
         </div>
       )}
